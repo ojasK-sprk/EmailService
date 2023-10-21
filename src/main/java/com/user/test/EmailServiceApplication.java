e6244dfad4aa3d337c091cde523ae4eb4457f6d6
ojasK-sprk <dev7@sprktechnologies.in>
2023-10-21 12:32:15 +0530

    Encrypted credentials for mail using jasypt
    Added dependency and plugin for jasypt in pom.xml and added encryptor key in application configuration vm

--- src/main/java/com/user/test/EmailServiceApplication.java
+++ src/main/java/com/user/test/EmailServiceApplication.java
@@ -1,15 +1,14 @@
 package com.user.test;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import com.user.test.entity.EmailDetails;
 import com.user.test.service.EmailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EmailServiceApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
